5a3dfc0fbbbb66ce782a5b1b6bdbf38bdbe249ea
Kalle <kalle@kandru.de>
2025-03-13 16:39:52 +0100

    bugfix: do not crash on map change

--- src/DemoRecorder.cs
+++ src/DemoRecorder.cs
@@ -1,8 +1,9 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
 
 namespace DemoRecorder;
-    
+
 public partial class DemoRecorder : BasePlugin
 {
     public override string ModuleName => "Demo Recorder";
@@ -10,56 +11,108 @@ public partial class DemoRecorder : BasePlugin
 
     private string _demoFolder = "addons/counterstrikesharp/data/demos";
     private bool _isRecording = false;
-    
+
     public override void Load(bool hotReload)
     {
         // create directory for demos
         Directory.SetCurrentDirectory(Server.GameDirectory);
         Directory.CreateDirectory($"csgo/{_demoFolder}");
         // register listener
+        AddCommandListener("changelevel", CommandListener_Changelevel, HookMode.Pre);
+        AddCommandListener("ds_workshop_changelevel", CommandListener_Changelevel, HookMode.Pre);
         RegisterEventHandler<EventRoundStart>(OnRoundStart);
+        RegisterEventHandler<EventPlayerConnectFull>(OnPlayerConnectFull);
+        RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
         RegisterEventHandler<EventCsIntermission>(OnCsIntermission);
         RegisterEventHandler<EventCsWinPanelMatch>(OnCsWinPanelMatch);
         RegisterEventHandler<EventCsMatchEndRestart>(OnCsMatchEndRestart);
+        if (hotReload)
+        {
+            if (PlayersConnected())
+                StartRecording();
+        }
     }
 
     public void Unload()
     {
         // stop recording
         StopRecording();
+        // deregister listener
+        RemoveCommandListener("changelevel", CommandListener_Changelevel, HookMode.Pre);
+        RemoveCommandListener("ds_workshop_changelevel", CommandListener_Changelevel, HookMode.Pre);
+        DeregisterEventHandler<EventRoundStart>(OnRoundStart);
+        DeregisterEventHandler<EventPlayerConnectFull>(OnPlayerConnectFull);
+        DeregisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
+        DeregisterEventHandler<EventCsIntermission>(OnCsIntermission);
+        DeregisterEventHandler<EventCsWinPanelMatch>(OnCsWinPanelMatch);
+        DeregisterEventHandler<EventCsMatchEndRestart>(OnCsMatchEndRestart);
+    }
+
+    private HookResult CommandListener_Changelevel(CCSPlayerController? player, CommandInfo commandInfo)
+    {
+        if (_isRecording && commandInfo.ArgCount >= 2)
+        {
+            StopRecording();
+            string command = commandInfo.GetArg(0);
+            string map = commandInfo.GetArg(1);
+            AddTimer(3.0f, () => Server.ExecuteCommand($"{command} {map}"));
+            return HookResult.Stop;
+        }
+
+        return HookResult.Continue;
     }
 
     private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
     {
-        if (_isRecording) return HookResult.Continue;
+        if (PlayersConnected())
+            StartRecording();
+        return HookResult.Continue;
+    }
+
+    private HookResult OnPlayerConnectFull(EventPlayerConnectFull @event, GameEventInfo info)
+    {
         StartRecording();
+        return HookResult.Continue;
+    }
+
+    private HookResult OnPlayerDisconnect(EventPlayerDisconnect @event, GameEventInfo info)
+    {
+        if (!PlayersConnected())
+            StopRecording();
         return HookResult.Continue;
     }
 
     private HookResult OnCsIntermission(EventCsIntermission @event, GameEventInfo info)
     {
+        StopRecording();
         return HookResult.Continue;
     }
 
     private HookResult OnCsWinPanelMatch(EventCsWinPanelMatch @event, GameEventInfo info)
     {
-        if (!_isRecording) return HookResult.Continue;
+
         StopRecording();
         return HookResult.Continue;
     }
 
     private HookResult OnCsMatchEndRestart(EventCsMatchEndRestart @event, GameEventInfo info)
     {
-        if (!_isRecording) return HookResult.Continue;
         StopRecording();
         return HookResult.Continue;
     }
- 
+
+    private bool PlayersConnected()
+    {
+        return Utilities.GetPlayers().Count(player => !player.IsBot) > 0;
+    }
+
     private void StartRecording()
     {
-        if ( _isRecording) return;
+        if (_isRecording) return;
         _isRecording = true;
-        string demoName = DateTime.Now.ToString("dd_MM_yyyy_HH_mm") + "-" + Server.MapName.ToLower() + ".dem";
+        string demoName = DateTime.Now.ToString("yyyy_MM_dd_HH_mm") + "-" + Server.MapName.ToLower() + ".dem";
+        Server.ExecuteCommand($"tv_enable 1");
+        Server.ExecuteCommand($"tv_record_immediate 1");
         Server.ExecuteCommand($"tv_record \"{_demoFolder}/{demoName}\"");
     }
 
