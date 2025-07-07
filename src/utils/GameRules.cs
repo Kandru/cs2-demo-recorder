fad9d5a7de0440508cdd81e61cbd8555a487e518
Kalle <kalle@kandru.de>
2025-07-07 10:54:36 +0200

    code cleanup

--- src/utils/GameRules.cs
+++ src/utils/GameRules.cs
@@ -1,14 +1,11 @@
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Modules.Entities.Constants;
-using CounterStrikeSharp.API.Modules.Utils;
 
 namespace DemoRecorder.Utils
 {
     public static class GameRules
     {
         public static CCSGameRules? _gameRules;
-        private static IEnumerable<CCSTeam>? _teamManager;
 
         private static CCSGameRules? GetGameRule()
         {
@@ -22,49 +19,6 @@ namespace DemoRecorder.Utils
             _ = GetGameRule();
             System.Reflection.PropertyInfo? property = _gameRules?.GetType().GetProperty(rule);
             return property?.CanRead == true ? property.GetValue(_gameRules) : null;
-        }
-
-        public static void SetRoundTime(float minutes)
-        {
-            _ = GetGameRule();
-            if (_gameRules != null)
-            {
-                _gameRules.RoundTime = (int)Math.Round(minutes * 60);
-            }
-        }
-
-        public static void TerminateRound(RoundEndReason reason, float delay = 0f)
-        {
-            _ = GetGameRule();
-            if (_gameRules != null)
-            {
-                _gameRules.RoundsPlayedThisPhase = 1;
-                _gameRules.ITotalRoundsPlayed = 1;
-                _gameRules.TotalRoundsPlayed = 1;
-                _gameRules.TerminateRound(delay, reason);
-            }
-        }
-
-        public static void SetTeamScore(int score, CsTeam team)
-        {
-            if (_teamManager == null)
-            {
-                _teamManager = Utilities.FindAllEntitiesByDesignerName<CCSTeam>("cs_team_manager");
-                if (_teamManager == null)
-                {
-                    return;
-                }
-            }
-
-            foreach (CCSTeam entry in _teamManager)
-            {
-                if (entry.TeamNum == (byte)team)
-                {
-                    entry.Score = score;
-                    Utilities.SetStateChanged(entry, "CTeam", "m_iScore");
-                    break;
-                }
-            }
         }
     }
 }
